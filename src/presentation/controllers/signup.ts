38e4458cdda1e3e7815e22c085afef4bc5bc87ed
Michel <silvaalvesmichel07@outlook.com>
2021-02-25 20:24:26 -0300

    chore: add EmailValidator

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,17 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
-import {Controller} from '../protocols/controller'
+import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
-export class SignUpController implements Controller{
+export class SignUpController implements Controller {
+
+    private emailValidator: EmailValidator
+
+    constructor(emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator
+    }
 
     handle(httpRequest: HttpRequest): HttpResponse {
         const requiredFilds = ['name', 'email', 'password', 'password', 'passwordConfirmation']
@@ -11,6 +19,10 @@ export class SignUpController implements Controller{
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
             }
+        }
+
+        if (!this.emailValidator.isValid(httpRequest.body.email)) {
+            return badRequest(new InvalidParamError('email'))
         }
 
     }
