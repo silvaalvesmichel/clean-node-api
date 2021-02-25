313c3586c7cc92afab9d382aac0fc5684a291411
Michel <silvaalvesmichel07@outlook.com>
2021-02-25 19:34:27 -0300

    chore: add test password and passwordConfirmation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,7 +5,7 @@ import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
 
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFilds = ['name', 'email', 'password']
+        const requiredFilds = ['name', 'email', 'password', 'password', 'passwordConfirmation']
         for (const field of requiredFilds) {
             if (!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
