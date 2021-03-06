d3883191b803190d1d04c99d3cbe46946ff9745f
Michel <silvaalvesmichel07@outlook.com>
2021-03-06 16:03:40 -0300

    refactor: move server Erro to a help method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,9 @@
-import { HttpResponse, HttpRequest } from '../protocols/http'
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
 
@@ -28,10 +27,7 @@ export class SignUpController implements Controller {
             }
 
         } catch (error) {
-            return {
-                statusCode: 500,
-                body: new ServerError()
-            }
+            return serverError()
         }
     }
 }
