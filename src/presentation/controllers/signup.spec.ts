cba8a9bee82c79da44583f30394c23c03864561b
Michel <silvaalvesmichel07@outlook.com>
2021-03-07 13:07:55 -0300

    chore: add validation passwordConfirmation and test passwordConfirmation

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -63,7 +63,7 @@ describe('SignUp Controller', () => {
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
 
-    test('should return 400 if no email is provided', () => {
+    test('should return 400 if no password is provided', () => {
         const { sut } = makeSut()
         const httpRequest = {
             body: {
@@ -77,7 +77,22 @@ describe('SignUp Controller', () => {
         expect(httpResponse.body).toEqual(new MissingParamError('password'))
     })
 
-    test('should return 400 if no email is provided', () => {
+    test('should return 400 if no passwordConfirmation fails', () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'invalid_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+    })
+
+    test('should return 400 if no passwordConfirmation is provided', () => {
         const { sut } = makeSut()
         const httpRequest = {
             body: {
@@ -140,6 +155,9 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(500)
         expect(httpResponse.body).toEqual(new ServerError())
     })
+
+
+    
 
 
 })
